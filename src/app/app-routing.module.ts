9c4fa63d4cf923307f7ccc3f0f1eaabf18c10f16
Gerard <gerardgg02@gmail.com>
2020-12-09 15:36:44 +0100

    Request list created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { AboutComponent } from './about/about.component';
 import { NotFoundComponent } from './error-handler/error-alert/not-found.component';
 import { UserListComponent } from './user/user-list/user-list.component';
 import { UserDetailComponent } from './user/user-detail/user-detail.component';
-import { UserRegisterComponent } from './user/user-register/user-register.component';
 import { UserEditComponent } from './user/user-edit/user-edit.component';
 import { UserDeleteComponent } from './user/user-delete/user-delete.component';
 import {DatasetCreateComponent} from './dataset/dataset-create/dataset-create.component';
