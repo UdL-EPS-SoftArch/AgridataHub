4e5d1950233d680c2bf83c3f85f899d87d8199d0
Jorge Nueno Abril <59286486+jorgics@users.noreply.github.com>
2020-12-22 16:07:37 +0100

    Delete user-search and userservice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,13 +14,11 @@ import { LoggedInGuard } from './login-basic/loggedin.guard';
 import { AuthInterceptor } from './login-basic/auth-interceptor';
 
 import { AuthenticationBasicService } from './login-basic/authentication-basic.service';
-import { UserService } from './user/user.service';
-import {ReuserService} from './reuser/reuser.service';
-import {ProviderService} from './provider/provider.service';
+import { ReuserService } from './reuser/reuser.service';
+import { ProviderService } from './provider/provider.service';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { UserSearchComponent } from './user/user-search/user-search.component';
 import { DatasetCreateComponent } from './dataset/dataset-create/dataset-create.component';
 import { DatasetListComponent } from './dataset/dataset-list/dataset-list.component';
 import { ProviderRegisterComponent } from './provider/provider-register/provider-register.component';
@@ -50,9 +48,6 @@ import { DatasetModifyComponent } from './dataset/dataset-modify/dataset-modify.
 
 
 
-
-
-
 registerLocaleData(localeEs);
 
 @NgModule({
@@ -71,7 +66,6 @@ registerLocaleData(localeEs);
     ProviderDetailComponent,
     ProviderEditComponent,
     ProviderSearchComponent,
-    UserSearchComponent,
     DatasetCreateComponent,
     DatasetListComponent,
     DatasetDetailsComponent,
@@ -100,7 +94,7 @@ registerLocaleData(localeEs);
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
     { provide: 'ExternalConfigurationService', useClass: ExternalConfigurationService },
-    AuthenticationBasicService, LoggedInGuard, UserService, ProviderService, ReuserService,
+    AuthenticationBasicService, LoggedInGuard, ProviderService, ReuserService,
   ],
   bootstrap: [AppComponent]
 })
